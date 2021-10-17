8c09773e6587edd3f4f0ae59c4a253aee92e5015
Bruno Durante <brunodurantec@gmail.com>
2021-10-17 11:34:45 -0300

    Including basic mechanics of Hangman game

--- games/forca.py
+++ games/forca.py
@@ -3,11 +3,22 @@ def jogar():
     print("**** Bem vindo ao jogo de Forca ****")
     print("************************************")
 
-    print("(1) Fácil (2) Médio (3) Difícil")
-    nivel = int(input("Selecione a dificuldade: "))
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+    index = 0
+
+    while(not enforcou and not acertou):
+        chute = input("Digite uma letra: ")
+        chute = chute.strip()
+
+        for letra in palavra_secreta:
+            if(chute.lower() == letra.lower()):
+                print("Letra {} encontrada na posição {}.".format(chute, index))
+            index += 1
 
     print("Fim de jogo!")
 
-#Necessário para execução via Command
+#Necessário para execução via Command. Verifica se a execução foi startada a partir dessa classe.
 if (__name__ == "__main__"):
     jogar()
