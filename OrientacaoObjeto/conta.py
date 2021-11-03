95b716b9bb68b7891e25cdc038b153d33de20b80
Bruno Durante <brunodurantec@gmail.com>
2021-11-02 22:15:22 -0300

    Inserindo metodos na classe

--- OrientacaoObjeto/conta.py
+++ OrientacaoObjeto/conta.py
@@ -1,6 +1,16 @@
+# Os testes foram feitos através do Python Console
 class Conta:
     def __init__(self, numero, titular, saldo, limite=1000.0):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
+
+    def extrato(self):
+        print("Saldo é de R${}".format(self.saldo))
